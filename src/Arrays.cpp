a4b18da39866924fb0979fd57bdf916e3382781c
Christopher Hiew <christopherhiew@protonmail.com>
2022-11-13 20:14:33 +0000

    Updates to .gitignore file.

---
+++ src/Arrays.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Arrays.cpp
+// Author      : Christopher Hiew
+// Version     :
+// Copyright   :
+// Description :
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "The start of something (allegedly) beautiful..." << endl;
+	return 0;
+}
